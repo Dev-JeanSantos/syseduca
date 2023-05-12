0fb6896136c7fb5cba224a7fe564c0802e99438b
Dev-JeanSantos <jeancbsan@gmail.com>
2023-05-12 19:16:23 -0300

    Starter Project

---
+++ backend/src/test/java/com/fourtk/syseduca/SyseducaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourtk.syseduca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyseducaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
