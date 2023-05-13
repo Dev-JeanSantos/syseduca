bbafd7c06e1182d0dc16293736e41943a322f6b6
Dev-JeanSantos <jeancbsan@gmail.com>
2023-05-13 09:52:25 -0300

    created dominio class Institution

---
+++ backend/src/main/java/com/fourtk/syseduca/controllers/InstitutionController.java
@@ -0,0 +1,14 @@
+package com.fourtk.syseduca.controllers;
+
+
+import com.fourtk.syseduca.models.Institution;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/institutions")
+public class InstitutionController {
+
+}
---
+++ backend/src/main/java/com/fourtk/syseduca/models/Institution.java
@@ -0,0 +1,142 @@
+package com.fourtk.syseduca.models;
+
+import java.util.Objects;
+
+public class Institution {
+    private Long id;
+    private String name;
+    private String cnpj;
+    private String stateRegistration;
+    private String address;
+    private String postalCode;
+    private String email;
+    private String city;
+    private char uf;
+    private String complement;
+    private String phone;
+
+    public Institution(Long id,
+                       String name,
+                       String cnpj,
+                       String stateRegistration,
+                       String address,
+                       String postalCode,
+                       String email,
+                       String city,
+                       char uf,
+                       String complement,
+                       String phone) {
+        this.id = id;
+        this.name = name;
+        this.cnpj = cnpj;
+        this.stateRegistration = stateRegistration;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.email = email;
+        this.city = city;
+        this.uf = uf;
+        this.complement = complement;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getStateRegistration() {
+        return stateRegistration;
+    }
+
+    public void setStateRegistration(String stateRegistration) {
+        this.stateRegistration = stateRegistration;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public char getUf() {
+        return uf;
+    }
+
+    public void setUf(char uf) {
+        this.uf = uf;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Institution that = (Institution) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
